fcb53f47810f0465764bb43a0a23ddcaa060f763
as1n <novruzo.aslan.as@yandex.ru>
2019-03-03 19:26:40 +0300

    Lab1

---
+++ lab1/src/main/java/com/netcracker/edu/backend/lab1/controller/Controller.java
@@ -0,0 +1,28 @@
+package com.netcracker.edu.backend.lab1.controller;
+
+import com.netcracker.edu.backend.lab1.entity.Entity;
+import com.netcracker.edu.backend.lab1.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/controller")
+public class Controller {
+
+    @Autowired
+    Service service;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<Entity> getEntity(
+            @RequestParam(name = "num") Integer num
+    ) {
+//        Entity entity = new Entity(service.getEven(num), service.getSimple(num));
+        return ResponseEntity.ok(new Entity(service.getEven(num), service.getSimple(num)));
+    }
+
+    
+}
---
+++ lab1/src/main/java/com/netcracker/edu/backend/lab1/entity/Entity.java
@@ -0,0 +1,30 @@
+package com.netcracker.edu.backend.lab1.entity;
+
+public class Entity {
+    private boolean isEven;
+    private boolean isSimple;
+
+    public Entity() {
+    }
+
+    public Entity(boolean isEven, boolean isSimple) {
+        this.isEven = isEven;
+        this.isSimple = isSimple;
+    }
+
+    public boolean isEven() {
+        return isEven;
+    }
+
+    public void setEven(boolean even) {
+        isEven = even;
+    }
+
+    public boolean isSimple() {
+        return isSimple;
+    }
+
+    public void setSimple(boolean simple) {
+        isSimple = simple;
+    }
+}
